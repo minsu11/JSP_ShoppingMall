bae882cc2137391a91d4dc195c9ab8963cf82051
minsu <zmfjtnl1@naver.com>
2023-12-17 20:14:04 +0900

    Upload UserRepositoryImpl.java Optional findByUserIdAndUserPassword and findById method

--- src/main/java/com/nhnacademy/shoppingmall/user/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/nhnacademy/shoppingmall/user/repository/impl/UserRepositoryImpl.java
@@ -3,44 +3,43 @@ package com.nhnacademy.shoppingmall.user.repository.impl;
 import com.nhnacademy.shoppingmall.common.mvc.transaction.DbConnectionThreadLocal;
 import com.nhnacademy.shoppingmall.user.domain.User;
 import com.nhnacademy.shoppingmall.user.repository.UserRepository;
-import lombok.extern.slf4j.Slf4j;
-
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDateTime;
 import java.util.Objects;
 import java.util.Optional;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public Optional<User> findByUserIdAndUserPassword(String userId, String userPassword) {
-        /*todo#3-1 회원의 아이디와 비밀번호를 이용해서 조회하는 코드 입니다.(로그인)
-          해당 코드는 SQL Injection이 발생합니다. SQL Injection이 발생하지 않도록 수정하세요.
-         */
         Connection connection = DbConnectionThreadLocal.getConnection();
-        String sql =String.format("select user_id, user_name, user_password, user_birth, user_auth, user_point, created_at, latest_login_at from users where user_id='%s' and user_password ='%s'",
-                userId,
-                userPassword
-        );
+        String sql = "select user_id, user_name, user_password, user_birth, user_auth, user_point, created_at, latest_login_at from users where user_id=? and user_password =?";
 
-        log.debug("sql:{}",sql);
+        log.debug("sql:{}", sql);
 
-        try( Statement psmt = connection.createStatement();
-             ResultSet rs =  psmt.executeQuery(sql);
+        try (PreparedStatement psmt = connection.prepareStatement(sql);
         ) {
-            if(rs.next()){
-                User user = new User(
-                        rs.getString("user_id"),
-                        rs.getString("user_name"),
-                        rs.getString("user_password"),
-                        rs.getString("user_birth"),
-                        User.Auth.valueOf(rs.getString("user_auth")),
-                        rs.getInt("user_point"),
-                        Objects.nonNull(rs.getTimestamp("created_at")) ? rs.getTimestamp("created_at").toLocalDateTime() : null,
-                        Objects.nonNull(rs.getTimestamp("latest_login_at")) ? rs.getTimestamp("latest_login_at").toLocalDateTime() : null
-                );
-                return Optional.of(user);
+            psmt.setString(1, userId);
+            psmt.setString(2, userPassword);
+            try (ResultSet rs = psmt.executeQuery()) {
+                if (rs.next()) {
+                    User user = new User(
+                            rs.getString("user_id"),
+                            rs.getString("user_name"),
+                            rs.getString("user_password"),
+                            rs.getString("user_birth"),
+                            User.Auth.valueOf(rs.getString("user_auth")),
+                            rs.getInt("user_point"),
+                            Objects.nonNull(rs.getTimestamp("created_at")) ? rs.getTimestamp("created_at").toLocalDateTime() : null,
+                            Objects.nonNull(rs.getTimestamp("latest_login_at")) ? rs.getTimestamp("latest_login_at").toLocalDateTime() : null
+                    );
+                    return Optional.of(user);
+                }
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -51,37 +50,60 @@ public class UserRepositoryImpl implements UserRepository {
 
     @Override
     public Optional<User> findById(String userId) {
-        //todo#3-2 회원조회
+
+        Connection connection = DbConnectionThreadLocal.getConnection();
+        String sql = "select user_id, user_name, user_password, user_birth, user_auth, user_point, created_at, latest_login_at from users where user_id=?";
+
+        log.debug("sql:{}", sql);
+
+        try (PreparedStatement psmt = connection.prepareStatement(sql);
+        ) {
+            psmt.setString(1, userId);
+            try (ResultSet rs = psmt.executeQuery()) {
+                if (rs.next()) {
+                    User user = new User(
+                            rs.getString("user_id"),
+                            rs.getString("user_name"),
+                            rs.getString("user_password"),
+                            rs.getString("user_birth"),
+                            User.Auth.valueOf(rs.getString("user_auth")),
+                            rs.getInt("user_point"),
+                            Objects.nonNull(rs.getTimestamp("created_at")) ? rs.getTimestamp("created_at").toLocalDateTime() : null,
+                            Objects.nonNull(rs.getTimestamp("latest_login_at")) ? rs.getTimestamp("latest_login_at").toLocalDateTime() : null
+                    );
+                    return Optional.of(user);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return Optional.empty();
     }
 
     @Override
     public int save(User user) {
-        //todo#3-3 회원등록, executeUpdate()을 반환합니다.
+
         return 0;
     }
 
     @Override
     public int deleteByUserId(String userId) {
-        //todo#3-4 회원삭제, executeUpdate()을 반환합니다.
         return 0;
     }
 
     @Override
     public int update(User user) {
-        //todo#3-5 회원수정, executeUpdate()을 반환합니다.
         return 0;
     }
 
     @Override
     public int updateLatestLoginAtByUserId(String userId, LocalDateTime latestLoginAt) {
-        //todo#3-6, 마지막 로그인 시간 업데이트, executeUpdate()을 반환합니다.
         return 0;
     }
 
     @Override
     public int countByUserId(String userId) {
-        //todo#3-7 userId와 일치하는 회원의 count를 반환합니다.
         return 0;
     }
 
