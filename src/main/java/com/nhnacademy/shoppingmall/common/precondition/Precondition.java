9a992efe9b7f6fed1a0e917e22889254d6d3dfd7
minsu <zmfjtnl1@naver.com>
2023-12-17 20:08:47 +0900

    Upload Precondition.java void isCheckNull method

--- src/main/java/com/nhnacademy/shoppingmall/common/precondition/Precondition.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/precondition/Precondition.java
@@ -4,7 +4,18 @@ import java.util.Objects;
 
 public final class Precondition {
 
-    
+    public static <T> void isCheckNull(T value) {
+        if (isNull(value)) {
+            throw new NullPointerException();
+        }
+    }
+
+    public static <T> void isCheckNull(T value, Object format) {
+        if (isNull(value)) {
+            throw new NullPointerException(String.valueOf(format));
+        }
+    }
+
     private static <T> boolean isNull(T value) {
         return Objects.isNull(value);
     }
