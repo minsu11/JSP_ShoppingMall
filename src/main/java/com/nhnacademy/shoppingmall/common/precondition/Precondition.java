9da8f28aab267827c291cea427b72fcf2d62444f
minsu <zmfjtnl1@naver.com>
2023-12-17 20:06:32 +0900

    Upload Precondition.java boolean isNull method

--- src/main/java/com/nhnacademy/shoppingmall/common/precondition/Precondition.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/precondition/Precondition.java
@@ -1,2 +1,11 @@
-package com.nhnacademy.shoppingmall.common.precondition;public class Precondition {
+package com.nhnacademy.shoppingmall.common.precondition;
+
+import java.util.Objects;
+
+public final class Precondition {
+
+    
+    private static <T> boolean isNull(T value) {
+        return Objects.isNull(value);
+    }
 }
