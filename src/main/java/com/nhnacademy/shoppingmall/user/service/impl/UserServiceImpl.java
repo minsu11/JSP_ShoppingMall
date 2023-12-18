7450fdd8c070b7683b04fc4d487818fa1f04da96
minsu <zmfjtnl1@naver.com>
2023-12-18 13:28:44 +0900

    Upload UserServiceImpl all method

--- src/main/java/com/nhnacademy/shoppingmall/user/service/impl/UserServiceImpl.java
+++ src/main/java/com/nhnacademy/shoppingmall/user/service/impl/UserServiceImpl.java
@@ -1,9 +1,16 @@
 package com.nhnacademy.shoppingmall.user.service.impl;
 
-import com.nhnacademy.shoppingmall.user.service.UserService;
+import com.nhnacademy.shoppingmall.common.precondition.Precondition;
 import com.nhnacademy.shoppingmall.user.domain.User;
+import com.nhnacademy.shoppingmall.user.exception.UserAlreadyExistsException;
+import com.nhnacademy.shoppingmall.user.exception.UserNotFoundException;
 import com.nhnacademy.shoppingmall.user.repository.UserRepository;
+import com.nhnacademy.shoppingmall.user.service.UserService;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
 
@@ -12,31 +19,47 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public User getUser(String userId){
-        //todo#4-1 회원조회
-        return null;
+    public User getUser(String userId) {
+        Precondition.isCheckNull(userId);
+        User user = userRepository.findById(userId).orElse(null);
+        if (Objects.isNull(user)) {
+            log.debug("user:{}", user);
+            return null;
+        }
+        return user;
     }
 
     @Override
     public void saveUser(User user) {
-        //todo#4-2 회원등록
-
+        if (userRepository.countByUserId(user.getUserId()) < 1) {
+            userRepository.save(user);
+        }
+        throw new UserAlreadyExistsException("user");
     }
 
     @Override
     public void updateUser(User user) {
-        //todo#4-3 회원수정
+        if (userRepository.countByUserId(user.getUserId()) > 0) {
+            userRepository.update(user);
+        }
     }
 
     @Override
     public void deleteUser(String userId) {
-        //todo#4-4 회원삭제
+        if (userRepository.countByUserId(userId) > 0) {
+            userRepository.deleteByUserId(userId);
+        }
+
     }
 
     @Override
     public User doLogin(String userId, String userPassword) {
-        //todo#4-5 로그인 구현, userId, userPassword로 일치하는 회원 조회
-        return null;
+        User user = userRepository.findByUserIdAndUserPassword(userId, userPassword).orElse(null);
+        if (Objects.nonNull(user)) {
+            userRepository.updateLatestLoginAtByUserId(user.getUserId(), LocalDateTime.now());
+            return user;
+        }
+        throw new UserNotFoundException("login fail");
     }
 
 }
