c808a6de271b604a88bf74027bc2f629453e5526
minsu <zmfjtnl1@naver.com>
2023-12-19 12:47:45 +0900

    Refactor ControllerFactory.java remove javadoc

--- src/main/java/com/nhnacademy/shoppingmall/common/mvc/controller/ControllerFactory.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/mvc/controller/ControllerFactory.java
@@ -24,17 +24,6 @@ public class ControllerFactory {
             return;
         }
 
-        /*todo#5-1 ControllerFactory 초기화, 아래 설명을 참고하여 구현합니다.
-         * 1. Set<Class<?>> c 에는 com.nhnacademy.shoppingmall.common.initialize.WebAppInitializer 에서  HandlesTypes에
-         * com.nhnacademy.shoppingmall.common.mvc.controller.BaseController.class인 class를 set에 담겨서 parameter로 전달 됩니다.
-         * BaseController를 구현한 Controller class가 전달됩니다.
-         *
-         * 2.Java Reflection API를 사용하여 Controller class의 instance를 생성하고 beanMap에 등록합니다. key/value는 다음과 같습니다.
-         *  ex) key= GET-/index.do , value = IndexController's instance
-         *
-         * 3. @RequestMapping(method = RequestMapping.Method.GET,value = {"/index.do","/main.do"}) 처럼 value는 String 배열일 수 있습니다.
-         *  즉 /index.do, /main.do -> IndexController로 맵핑 됩니다.
-         */
         try {
             for (Class<?> clazz : c) {
                 RequestMapping.Method method = clazz.getAnnotation(RequestMapping.class).method();
@@ -49,24 +38,20 @@ public class ControllerFactory {
             throw new RuntimeException(e);
         }
 
-        //#todo5-2 ctx(ServletContext)에  attribute를 추가합니다. -> key : CONTEXT_CONTROLLER_FACTORY_NAME, value : ControllerFactory
         ctx.setAttribute(CONTEXT_CONTROLLER_FACTORY_NAME, this);
 
     }
 
     private Object getBean(String key) {
-        //todo#5-3 beanMap에서 controller 객체를 반환 합니다.
-        Precondition.isCheckNull(key);
         return beanMap.get(key);
     }
 
     public Object getController(HttpServletRequest request) {
-        //todo#5-4 request의 method, servletPath를 이용해서 Controller 객체를 반환합니다.
+        Precondition.isCheckNull(request);
         return getBean(getKey(request.getMethod(), request.getServletPath()));
     }
 
     public Object getController(String method, String path) {
-        //todo#5-5 method, path를 이용해서 Controller 객체를 반환 합니다.
         Precondition.isCheckNull(method);
         Precondition.isCheckNull(path);
         Object controller = getBean(getKey(method, path));
@@ -77,10 +62,6 @@ public class ControllerFactory {
     }
 
     private String getKey(String method, String path) {
-        //todo#5-6  {method}-{key}  형식으로 Key를 반환 합니다.
-        //ex GET-/index.do
-        //ex POST-/loginAction.do
-
         return method + "-" + path;
     }
 }
