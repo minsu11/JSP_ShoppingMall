974f724021a4961faa0bef10d023bb604e82a873
minsu <zmfjtnl1@naver.com>
2023-12-22 10:28:58 +0900

    Upload FrontServlet.java void service method

--- src/main/java/com/nhnacademy/shoppingmall/common/mvc/servlet/FrontServlet.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/mvc/servlet/FrontServlet.java
@@ -1,5 +1,7 @@
 package com.nhnacademy.shoppingmall.common.mvc.servlet;
 
+import static javax.servlet.RequestDispatcher.*;
+
 import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
 import com.nhnacademy.shoppingmall.common.mvc.controller.ControllerFactory;
 import com.nhnacademy.shoppingmall.common.mvc.transaction.DbConnectionThreadLocal;
@@ -27,17 +29,14 @@ public class FrontServlet extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) {
         try {
-            //todo#7-3 Connection pool로 부터 connection 할당 받습니다. connection은 Thread 내에서 공유됩니다.
-
-
+            DbConnectionThreadLocal.initialize();
             BaseController baseController = (BaseController) controllerFactory.getController(req);
             String viewName = baseController.execute(req, resp);
 
             if (viewResolver.isRedirect(viewName)) {
                 String redirectUrl = viewResolver.getRedirectUrl(viewName);
                 log.debug("redirectUrl:{}", redirectUrl);
-                //todo#7-6 redirect: 로 시작하면  해당 url로 redirect 합니다.
-
+                resp.sendRedirect(viewName);
             } else {
                 String layout = viewResolver.getLayOut(viewName);
                 log.debug("viewName:{}", viewResolver.getPath(viewName));
@@ -48,10 +47,14 @@ public class FrontServlet extends HttpServlet {
         } catch (Exception e) {
             log.error("error:{}", e);
             DbConnectionThreadLocal.setSqlError(true);
-            //todo#7-5 예외가 발생하면 해당 예외에 대해서 적절한 처리를 합니다.
-
+            req.setAttribute("status_code", req.getAttribute(ERROR_STATUS_CODE));
+            req.setAttribute("exception_type", req.getAttribute(ERROR_EXCEPTION_TYPE));
+            req.setAttribute("message", req.getAttribute(ERROR_MESSAGE));
+            req.setAttribute("exception", req.getAttribute(ERROR_EXCEPTION));
+            req.setAttribute("request_uri", req.getAttribute(ERROR_REQUEST_URI));
+            log.error("status_code:{}", req.getAttribute(ERROR_STATUS_CODE));
         } finally {
-            //todo#7-4 connection을 반납합니다.
+            DbConnectionThreadLocal.reset();
 
         }
     }
