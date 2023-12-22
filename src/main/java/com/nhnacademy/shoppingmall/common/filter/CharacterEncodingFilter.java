354b565294ee4a11e8c1ec5b416a193dbd67ebf6
minsu <zmfjtnl1@naver.com>
2023-12-22 11:00:08 +0900

    Upload CharacterEncodingFilter

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/CharacterEncodingFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/CharacterEncodingFilter.java
@@ -1,18 +1,27 @@
 package com.nhnacademy.shoppingmall.common.filter;
 
+import java.io.IOException;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.annotation.WebInitParam;
-import java.io.IOException;
 
-public class CharacterEncodingFilter  implements Filter {
+@WebFilter(filterName = "characterEncodingFilter",
+        urlPatterns = "/*",
+        initParams = {@WebInitParam(name = "encoding", value = "UTF-8")})
+public class CharacterEncodingFilter implements Filter {
+    String value;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        value = filterConfig.getInitParameter("encoding");
+    }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         //todo#8 UTF-8 인코딩, initParams의 encoding parameter value값을 charset 으로 지정합니다.
-        //@WebFilter(initParams = {@WebInitParam(name = "encoding",value = "UTF-8")})
-
-        filterChain.doFilter(servletRequest,servletResponse);
+        servletRequest.setCharacterEncoding(value);
+        servletResponse.setCharacterEncoding(value);
+        filterChain.doFilter(servletRequest, servletResponse);
     }
 
 }
