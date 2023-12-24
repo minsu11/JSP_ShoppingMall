ec0c3a386644137e80c4e994dd047236d8e4c087
minsu <zmfjtnl1@naver.com>
2023-12-24 13:57:08 +0900

    Upload LoginCheckFilter.java void doFilter

--- src/main/java/com/nhnacademy/shoppingmall/common/filter/LoginCheckFilter.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/filter/LoginCheckFilter.java
@@ -1,19 +1,30 @@
 package com.nhnacademy.shoppingmall.common.filter;
 
-import lombok.extern.slf4j.Slf4j;
-
-import javax.servlet.*;
+import com.nhnacademy.shoppingmall.common.mvc.view.ViewResolver;
+import com.nhnacademy.shoppingmall.user.domain.User;
+import java.io.IOException;
+import java.util.Objects;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@WebFilter(filterName = "loginCheckFilter", urlPatterns = "/mypage/*")
 public class LoginCheckFilter extends HttpFilter {
     @Override
     protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
-        //todo#10 /mypage/ 하위경로의 접근은 로그인한 사용자만 접근할 수 있습니다.
-
+        HttpSession httpSession = req.getSession(false);
+        User user = (User) httpSession.getAttribute("login");
+        if (Objects.nonNull(user)) {
+            req.setAttribute(ViewResolver.LAYOUT_MYPAGE, "/WEB-INF/views/layout/mypage.jsp");
+            chain.doFilter(req, res);
+        } else {
+            res.sendRedirect("/login.do");
+        }
     }
 }
