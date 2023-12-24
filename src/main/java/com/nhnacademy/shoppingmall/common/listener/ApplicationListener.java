34aed7f7f1a07c76f25de8032e1579661e9221ce
minsu <zmfjtnl1@naver.com>
2023-12-24 14:52:23 +0900

    Refactor ApplicationListener.java

--- src/main/java/com/nhnacademy/shoppingmall/common/listener/ApplicationListener.java
+++ src/main/java/com/nhnacademy/shoppingmall/common/listener/ApplicationListener.java
@@ -19,7 +19,6 @@ public class ApplicationListener implements ServletContextListener {
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        //todo#12 application 시작시 테스트 계정인 admin,user 등록합니다. 만약 존재하면 등록하지 않습니다.
         log.debug("contextInitialized start");
         DbConnectionThreadLocal.initialize();
         if (Objects.isNull(userService.getUser("admin"))) { // admin 존재 시
