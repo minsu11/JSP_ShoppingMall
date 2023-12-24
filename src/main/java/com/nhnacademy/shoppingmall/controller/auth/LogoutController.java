58e3cd3a56b9753251a2630a2d3ff5232b38db49
minsu <zmfjtnl1@naver.com>
2023-12-24 16:12:28 +0900

    Upload LogoutController.java String execute method

--- src/main/java/com/nhnacademy/shoppingmall/controller/auth/LogoutController.java
+++ src/main/java/com/nhnacademy/shoppingmall/controller/auth/LogoutController.java
@@ -1,5 +1,22 @@
 package com.nhnacademy.shoppingmall.controller.auth;
 
-public class LogoutController {
-    //todo#13-3 로그아웃 구현
+import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
+import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
+import java.util.Objects;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequestMapping(method = RequestMapping.Method.GET, value = "/logoutAction.do")
+public class LogoutController implements BaseController {
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse resp) {
+        HttpSession httpSession = req.getSession(false);
+        if (Objects.nonNull(httpSession.getAttribute("login"))) {
+            httpSession.removeAttribute("login");
+        }
+        return "shop/main/index.do";
+    }
 }
