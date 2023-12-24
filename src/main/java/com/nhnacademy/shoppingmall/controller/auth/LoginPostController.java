5e089f85408f59504a5466d602386aab02686f7d
minsu <zmfjtnl1@naver.com>
2023-12-24 16:11:50 +0900

    Upload LoginPostController.java String execute method

--- src/main/java/com/nhnacademy/shoppingmall/controller/auth/LoginPostController.java
+++ src/main/java/com/nhnacademy/shoppingmall/controller/auth/LoginPostController.java
@@ -2,22 +2,35 @@ package com.nhnacademy.shoppingmall.controller.auth;
 
 import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
 import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
+import com.nhnacademy.shoppingmall.user.domain.User;
 import com.nhnacademy.shoppingmall.user.repository.impl.UserRepositoryImpl;
 import com.nhnacademy.shoppingmall.user.service.UserService;
 import com.nhnacademy.shoppingmall.user.service.impl.UserServiceImpl;
-
+import java.util.Objects;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 
-@RequestMapping(method = RequestMapping.Method.POST,value = "/loginAction.do")
+@Slf4j
+@RequestMapping(method = RequestMapping.Method.POST, value = "/loginAction.do")
 public class LoginPostController implements BaseController {
 
     private final UserService userService = new UserServiceImpl(new UserRepositoryImpl());
 
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) {
-        //todo#13-2 로그인 구현, session은 60분동안 유지됩니다.
-
-        return "shop/main/index";
+        String userId = req.getParameter("user_id");
+        String userPassword = req.getParameter("user_password");
+        User user = userService.doLogin(userId, userPassword);
+        log.debug("user:{}", user);
+        if (Objects.nonNull(user)) { // user 존재 시
+            HttpSession loginSession = req.getSession(true);
+            loginSession.setMaxInactiveInterval(3600);
+            loginSession.setAttribute("login", user);
+            log.debug("login session:{}", loginSession);
+            return "shop/main/index";
+        }
+        return "shop/login/login_form";
     }
 }
