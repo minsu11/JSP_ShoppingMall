0eb85fdd4a23d685773173c4edc29a3ba155f323
minsu11 <zmfjtnl1@naver.com>
2024-09-08 16:38:18 +0900

    feat: order 구현

--- src/main/java/com/nhnacademy/shoppingmall/order/domain/Order.java
+++ src/main/java/com/nhnacademy/shoppingmall/order/domain/Order.java
@@ -1,5 +1,10 @@
 package com.nhnacademy.shoppingmall.order.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
 /**
  * packageName    : com.nhnacademy.shoppingmall.order.domain
  * fileName       : Order
@@ -11,5 +16,12 @@ package com.nhnacademy.shoppingmall.order.domain;
  * -----------------------------------------------------------
  * 2024. 9. 8.        parkminsu       최초 생성
  */
+@Getter
+@NoArgsConstructor
 public class Order {
+    private Integer id;
+    private LocalDateTime orderDate;
+    private String address;
+    private LocalDateTime shipDate;
+
 }
