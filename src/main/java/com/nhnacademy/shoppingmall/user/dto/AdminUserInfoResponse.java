854649df7d79508dc54a0c2e37a40782c521596e
minsu11 <zmfjtnl1@naver.com>
2024-09-11 15:37:27 +0900

    feat: admin user info response

---
+++ src/main/java/com/nhnacademy/shoppingmall/user/dto/AdminUserInfoResponse.java
@@ -0,0 +1,24 @@
+package com.nhnacademy.shoppingmall.user.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * packageName    : com.nhnacademy.shoppingmall.user.dto
+ * fileName       : AdminUserInfoResponse
+ * author         : parkminsu
+ * date           : 2024. 9. 11.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024. 9. 11.        parkminsu       최초 생성
+ */
+@Getter
+@Builder
+public class AdminUserInfoResponse {
+    private String userId;
+    private String userName;
+    private String birth;
+}
