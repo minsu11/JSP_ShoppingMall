8f1d563d5f4aa8aef99eee8b722b1e0ef155bebb
minsu11 <zmfjtnl1@naver.com>
2024-09-16 15:46:41 +0900

    feat: product 생성

---
+++ src/main/java/com/nhnacademy/shoppingmall/product/domain/Product.java
@@ -0,0 +1,15 @@
+package com.nhnacademy.shoppingmall.product.domain;
+
+/**
+ * packageName    : com.nhnacademy.shoppingmall.product.domain
+ * fileName       : Product
+ * author         : parkminsu
+ * date           : 2024. 9. 16.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 2024. 9. 16.        parkminsu       최초 생성
+ */
+public class Product {
+}
