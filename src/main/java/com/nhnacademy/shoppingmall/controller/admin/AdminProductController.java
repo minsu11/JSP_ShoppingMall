8e8d5e715eeb142b835248ba939bd54fc41942c3
minsu11 <zmfjtnl1@naver.com>
2024-09-18 18:06:40 +0900

    feat: admin page product 페이지 구현

--- src/main/java/com/nhnacademy/shoppingmall/controller/admin/AdminProductController.java
+++ src/main/java/com/nhnacademy/shoppingmall/controller/admin/AdminProductController.java
@@ -1,11 +1,15 @@
 package com.nhnacademy.shoppingmall.controller.admin;
 
 import com.nhnacademy.shoppingmall.common.mvc.annotation.RequestMapping;
+import com.nhnacademy.shoppingmall.common.mvc.annotation.Transaction;
 import com.nhnacademy.shoppingmall.common.mvc.controller.BaseController;
+import com.nhnacademy.shoppingmall.product.dto.AdminPageProductInfo;
 import com.nhnacademy.shoppingmall.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * packageName    : com.nhnacademy.shoppingmall.controller.admin
@@ -18,14 +22,18 @@ import javax.servlet.http.HttpServletResponse;
  * -----------------------------------------------------------
  * 2024. 9. 16.        parkminsu       최초 생성
  */
+@Transaction
 @RequestMapping(method = RequestMapping.Method.GET, value = "/admin/product/list.do")
 public class AdminProductController implements BaseController {
+    private ProductService productService;
 
+    // 관리자 페이지에서 상품 목록을 보여주는 곳
     @Override
     public String execute(HttpServletRequest req, HttpServletResponse resp) {
-        ProductService productService =(ProductService) req.getServletContext().getAttribute("productService");
+        productService =(ProductService) req.getServletContext().getAttribute("productService");
 
-
+        List<AdminPageProductInfo> adminPageProductInfoList = productService.getAdminPageProductInfoList();
+        req.setAttribute("productList", adminPageProductInfoList);
 
         return "shop/admin/admin-product-list";
     }
